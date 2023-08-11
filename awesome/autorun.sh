c7f50a91598f3e46dd0a2915aefd6d358f207a85
Vetle-Dyngeland <vetle.dyngeland@gmail.com>
2023-08-11 14:20:03 +0200

    awesome stuff

---
+++ awesome/autorun.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+run() {
+    if ! pgrep -f "$1"; then
+        "$@" &
+    fi
+}
+
+# Place before any other application
+run "xrandr --output DP-1 --off" 
+
+# Boring applications
+run "picom" -b
+run "nitrogen"--restore
+run "xscreensaver" --no-splash
+run "nm-applet"
+
+# fun stuff
+run "steam"
+run "firefox"
+run "moosync"
+
+# Swap screens hack
+run "xrandr --output DP-1 --auto --left-of HDMI-0"
