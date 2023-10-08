0df45cdb620ada4b8792b44975b8cd7b4a61ae94
Vetle-Dyngeland <vetle.dyngeland@gmail.com>
2023-10-08 13:55:18 +0200

    awesome without vicious or catppuccin

--- awesome/ui/top-panel.lua
+++ awesome/ui/top-panel.lua
@@ -3,21 +3,21 @@ local awful = require("awful")
 
 local config = require("options.config")
 local widgets = require("widgets")
-local vicious = require("vicious")
+--local vicious = require("vicious")
 
-local function vicious_widgets()
-    local uptime = wibox.widget.textbox()
-    vicious.register(uptime, vicious.widgets.uptime, "up: $3m ", 10)
-
-    local date_widget = wibox.widget.textbox()
-    vicious.register(date_widget, vicious.widgets.date, "%b %d", 0.5)
-
-
-    return {
-        date = date_widget,
-        uptime = uptime
-    }
-end
+--local function vicious_widgets()
+--    local uptime = wibox.widget.textbox()
+--    vicious.register(uptime, vicious.widgets.uptime, "up: $3m ", 10)
+--
+--    local date_widget = wibox.widget.textbox()
+--    vicious.register(date_widget, vicious.widgets.date, "%b %d", 0.5)
+--
+--
+--    return {
+--        date = date_widget,
+--        uptime = uptime
+--    }
+--end
 
 local function shape(cr, width, height)
     return require("gears").shape.rounded_rect(cr, width, height, 7)
@@ -34,7 +34,7 @@ local function toppanel(s)
 end
 
 return function(s)
-    local v = vicious_widgets()
+--    local v = vicious_widgets()
 
     toppanel(s):setup({
         layout = wibox.layout.align.horizontal,
@@ -49,7 +49,7 @@ return function(s)
         widgets.tasklist(s),
         {
             layout = wibox.layout.fixed.horizontal,
-            v.uptime,
+ --           v.uptime,
             widgets.cpu,
             widgets.audio,
             widgets.textclock
