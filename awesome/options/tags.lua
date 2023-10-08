d533f6ff131d3d788e7d3dfdeb1b3d5f61522d9e
Vetle-Dyngeland <vetle.dyngeland@gmail.com>
2023-10-08 14:37:12 +0200

    generalized tags

--- awesome/options/tags.lua
+++ awesome/options/tags.lua
@@ -5,8 +5,8 @@ awful.layout.layouts = {
     awful.layout.suit.floating
 }
 
-awful.tag({ "code", "game", "steam", }, 1, awful.layout.layouts[1])
-awful.tag({ "main", "other", "spotify", "discord", }, 2, awful.layout.layouts[1])
+awful.tag({ "1", "2", "3", "4", "5", }, 1, awful.layout.layouts[1])
+awful.tag({ "1", "2", "3", "4", "5" }, 2, awful.layout.layouts[1])
 
 --awful.screen.connect_for_each_screen(function(s)
 --    awful.tag({ "1", "2", "3", "4", "5", }, s, awful.layout.layouts[1])
