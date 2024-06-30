4750358d08416ecc8a585af2691215dce5b10c16
Vetle Dyngeland <vetle.dyngeland@gmail.com>
2024-06-30 12:06:18 +0200

    remove rust, trying to fix issues

--- nvim/lua/plugins/lsp/rust.lua
+++ nvim/lua/plugins/lsp/rust.lua
@@ -1,34 +1,4 @@
 return {
-    --[[{
-        "mrcjkb/rusteceanvim",
-        version = "^4",
-        lazy = false,
-        config = function()
-            local bufnr = vim.api.nvim_get_current_buf()
-            vim.keymap.set("n", "<leader>a", function() vim.cmd.RustLsp("codeAction") end,
-                { silent = true, buffer = bufnr })
-
-            vim.g.rustaceanvim = {
-                server = {
-                    default_settings = {
-                        ["rust-analyzer"] = {
-                            cargo = {
-                                autoReload = true,
-                                allFeatures = true,
-                            },
-                            inlayHints = {
-                                maxLength = 50,
-                                closingBraceHints = {
-                                    enable = true,
-                                    minLines = 30
-                                },
-                            }
-                        }
-                    }
-                }
-            }
-        end
-    },]]
     {
         "Saecki/crates.nvim",
         dependencies = { "nvim-lua/plenary.nvim" },
